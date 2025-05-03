74eb0912bd5eaca9b4bb7dfe1ac1697a3dd9330b
SahrulMahdi23 <147253671+SahrulMahdi23@users.noreply.github.com>
2025-05-03 23:38:51 +0700

    Update SimulasiGame.java

--- SimulasiGame.java
+++ SimulasiGame.java
@@ -2,37 +2,37 @@
 //20230040137 – Polimorfisme (Overriding)
 class Karakter {
     public void menyerang() {
-        System.out.println("Karakter menyerang dengan tangan kosong!");
+        System.out.println("Karakter menyerang dengan kedua tangan kosong!");
     }
 }
 
 class Penyihir extends Karakter {
     @Override
     public void menyerang() {
-        System.out.println("Penyihir menyerang dengan magic power!");
+        System.out.println("Witch menyerang dengan magic power!");
     }
 }
 
 class Prajurit extends Karakter {
     @Override
     public void menyeerang() {
-        System.out.println("Prajurit menyerang dengan pedang dan perisai!");
+        System.out.println("Warior menyerang dengan pedang dan perisai!");
     }
 }
 
 class Pemanah extends Karakter {
     @Override
     public void menyeerang() {
-        System.out.println("Pemanah menyerang dengan panah dan busur panah!");
+        System.out.println("Archer menyerang dengan panah dan busur panah!");
     }
 }
 
 public class SimulasiGame {
     public static void main(String[] args) {
         Karakter[] karakter = {
-                new Penyihir(),
-                new Prajurit(),
-                new Pemanah()
+                new Witch(),
+                new Warior(),
+                new Archer()
         };
 
         for (Karakter k : karakter) {
